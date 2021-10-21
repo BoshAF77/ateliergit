9b89335cfddf5c7bc26b40de23b38f7bc16a2f81
BoshAF77 <anis.farjallah@esprit.tn>
2021-10-21 13:06:40 +0200

    MAJ main

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <string.h>
+int main()
+{
+    char A [100];
+    int longueur
+    prinf ("donner la chaine:" );
+    scanf ("%s");
+    longueur= strlen(A);
+    printf ("%s comporte %d caractères, A, longueur");
+    return (0)
